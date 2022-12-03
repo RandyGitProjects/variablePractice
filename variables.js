ace08ee9e2171c0bf7d9400b678e2b65a55cffeb
Randy Cisneros <rlcisneros90@gmail.com>
2022-12-02 19:29:30 -0700

    Show reassigned values

--- variables.js
+++ variables.js
@@ -7,3 +7,11 @@ var currentYear = 2022
 
 var tenGreaterThanNine = true
 var depositPaid = true
+
+nameFirst = "Maximus"
+nameLast = "Cisneros"
+nameMiddle = "Atlas"
+
+
+console.log(nameFirst, nameMiddle, nameLast)
+
